6789730eab1aa5c2bb6135a1f4bcfcffb3167310
WissalTa98 <wissal.talbi@esprit.tn>
2023-10-24 15:22:11 +0100

    all the project

---
+++ app_Business/Mapper/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using app_DataAccess;
+using app_Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_Business.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+
+        }
+    }
+}
---
+++ app_Business/Repository/CategoryRepository.cs
@@ -0,0 +1,64 @@
+﻿using app_Business.Repository.IRepository;
+using app_DataAccess;
+using app_DataAccess.Data;
+using app_Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_Business.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public CategoryRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public CategoryDTO Create(CategoryDTO objDTO)
+        {
+            Category category = new Category()
+            {
+                Name = objDTO.Name,
+                Id = objDTO.Id,
+                CreatedDate = DateTime.Now
+            };
+
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+
+            return new CategoryDTO()
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CategoryDTO Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CategoryDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CategoryDTO Update(CategoryDTO objDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
