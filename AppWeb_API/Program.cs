1d830b0bbe4ec39eac4d1b1f263da82c5ed2fa9e
WissalTa98 <wissal.talbi@esprit.tn>
2023-10-26 20:21:23 +0100

    Create API and adding Product Controller

---
+++ AppWeb_API/Program.cs
@@ -0,0 +1,37 @@
+using app_Business.Repository.IRepository;
+using app_Business.Repository;
+using app_DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseRouting();
+
+app.Run();
--- app_Models/ProductDTO.cs
+++ app_Models/ProductDTO.cs
@@ -23,5 +23,6 @@ namespace app_Models
 		public int CategoryId { get; set; }
 
 		public CategoryDTO Category { get; set; }
+		public ICollection<ProductPriceDTO> ProductPrices { get; set; }
 	}
 }
